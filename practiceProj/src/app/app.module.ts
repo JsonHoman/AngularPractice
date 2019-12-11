cf828d13253bb2f9d0323b41b8996982345f0d33
JsonHoman <homan.jason12@gmail.com>
2019-12-11 08:15:55 -0500

    update

--- practiceProj/src/app/app.module.ts
+++ practiceProj/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './home/products/products.component';
+import { DetailComponent } from './detail/detail.component';
+import { AdminComponent } from './admin/admin.component';
+import { CartComponent } from './navigation/cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    HomeComponent
+    HomeComponent,
+    ProductsComponent,
+    DetailComponent,
+    AdminComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
